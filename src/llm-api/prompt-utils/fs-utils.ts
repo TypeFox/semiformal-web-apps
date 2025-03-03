d983364e95214b60271c6a597cbcd507ac66315f
praisethemoon <doit@praisethemoon.org>
2025-03-03 13:15:11 +0100

    forcing notes to be in the base folder

--- src/llm-api/prompt-utils/fs-utils.ts
+++ src/llm-api/prompt-utils/fs-utils.ts
@@ -4,10 +4,12 @@ import { Logger } from '../../utils/logger.js';
 
 export async function createFile(baseFolder: string, folder: string, filename: string, content: string | object) {
     try {
-        let dist = filename.endsWith("NOTES.md") ? baseFolder : path.join(baseFolder, folder);
-        let fullPath = path.join(dist, filename);
+        let dist = path.join(baseFolder, folder);
+        
+        // incase of notes file, we override the folder to be the base folder
+        let fullPath = filename.endsWith("/NOTES.md") ? path.join(baseFolder, 'NOTES.md') : path.join(dist, filename);
 
-        if (filename.endsWith("NOTES.md")) {
+        if (filename.endsWith("/NOTES.md")) {
             Logger.info(`Creating notes file in ${fullPath}`);
         }
 
